f460a91427169a842a367984596d0a36debd7207
Gary Coburn <gcoburn@vmware.com>
2015-05-29 17:28:42 -0400

    Adding files

--- LightwaveScripts/LightwaveComponentInstall.sh
+++ LightwaveScripts/LightwaveComponentInstall.sh
@@ -14,11 +14,11 @@ fi
 
 #get the lightwave repo
 echo "Getting the lightwave.repo"
-wget --no-check-certificate https://raw.githubusercontent.com/gcoburn/AutomationScripts/master/lightwave.repo
+wget http://raw.githubusercontent.com/gcoburn/AutomationScripts/master/lightwave.repo
 
 #get the extras repo
 echo "Getting the photon-extras.repo"
-wget --no-check-certificate https://raw.githubusercontent.com/gcoburn/AutomationScripts/master/photon-extras.repo
+wget http://raw.githubusercontent.com/gcoburn/AutomationScripts/master/photon-extras.repo
 
 #edit the photon-iso.repo
 echo "Editing the photon-iso.repo"
@@ -27,7 +27,6 @@ sed -i 's/enabled=0/enabled=1/g' ./photon-iso.repo
 #install server optional
 echo "Installing components"
 $installCommand
-
 
 echo " "
 echo " "
